845b1e029025205f61ab3bc345e32f742c3306ed
Matthew Hansen <MHansen@Matthews-MacBook-Pro-2.local>
2013-04-24 13:59:58 -0700

    created base plugin and added in ui files

---
+++ kickstart.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: HTML Kickstart UI Library
+Plugin URI: http://flightofthedodo.com/Kickstart-wpplug
+Description: Extends Functionality from the HTML Kickstart (99lime.com) to any theme
+Version: 0.0.0 ALPHA
+Author: Matthew Hansen
+Author URI: http://flightofthedodo.com
+License: A "Slug" license name e.g. GPL2
+*/
+
+function load_my_kickstart_js() {  
+    wp_register_script('kickstart_js', plugins_url('/js/kickstart.js'), array("jquery"), '1.0', false );  
+    wp_enqueue_script('kickstart_js');  
+}  
+add_action('init', 'load_my_kickstart_js');  
+
+function load_my_kickstart_css()  {  
+    wp_register_style( 'kickstart_css', plugins_url('/css/kickstart.css'), array(), '20130423', 'all' );   
+    wp_enqueue_style( 'kickstart_css' );  
+}  
+add_action( 'wp_enqueue_scripts', 'load_my_kickstart_css' );  
+?>
